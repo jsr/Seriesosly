bd15569e694351dce5c01e83f347381b9fad6158
Jared Rosoff <jsr@10gen.com>
2011-04-05 20:00:23 -0700

    stupid rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
-require File.join(File.dirname(__FILE__), '..', 'seriesosly.rb')
+require File.join(File.dirname(__FILE__), '..', '/seriesosly.rb')
 
 require 'rspec'
 require 'rack/test'
-
-set :environment, :test 
 
 RSpec.configure do |conf| 
 	conf.include Rack::Test::Methods
