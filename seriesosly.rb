92758d22dfe74666a2613fc84a070db3ba85660b
Jared Rosoff <jsr@10gen.com>
2011-04-05 19:08:07 -0700

    Cleanup and ready for initial push

--- seriesosly.rb
+++ seriesosly.rb
@@ -1,5 +1,32 @@
 require 'rubygems'
-require 'sinatra'
-require 'yajl/json_gem'
+require 'sinatra/base'
+
+module Seriesosly
+	class Service < Sinatra::Base 
+		configure do 
+			set :app_file, __FILE__
+			set :run, Proc.new {app_file == $0}
+			set :environment, :development
+		end 
+
+		class << self 
+			def run!(options={})
+				init 
+				super 
+			end 
+
+			def init 
+				puts "Hello!"
+			end 
+		end 
 
 
+		get '/' do 
+			"Hello!"
+		end 
+
+
+		at_exit { run! if $!.nil? and run? }
+
+	end 
+end 
